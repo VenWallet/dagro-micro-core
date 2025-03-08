d43475d77666ab6f088f8bd28b5fe7983ce5ff68
Hector Palencia <hrpmicarelli@gmail.com>
2025-03-07 22:04:23 -0400

    feat: add image upload functionality with status endpoint and controller

---
+++ src/routes/image.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+const router = express.Router();
+import ImageController from '../controllers/image.controllers';
+import authMiddleware from '../middleware/auth.middleware';
+
+router.get('/status', ImageController.status)
+
+/**
+ * Post track
+ * @swagger
+ * /wallet/login-seed-phrase:
+ *    post:
+ *      tags:
+ *        - User
+ *      summary: .
+ *      description: .
+ *      requestBody:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: "object"
+ *                properties: {
+ *                }
+ *      responses:
+ *        '200':
+ *          description: .
+ *        '400':
+ *          description: .
+ *        '500':
+ *          description: server internal error.
+ */
+
+router.post('/upload-image', authMiddleware, ImageController.uploadImage);
+
+
+
+export { router }
